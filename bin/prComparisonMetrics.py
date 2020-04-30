f34ea2a9b452677f22f97c71354f5f02b1915c18
Tilak Sharma -X (tilsharm - ALTRAN ACT at Cisco) <tilsharm@partnet.cisco.com>
2020-04-30 18:28:19 +0530

    display table with metrics

--- bin/prComparisonMetrics.py
+++ bin/prComparisonMetrics.py
@@ -58,27 +58,27 @@ def buildTabularData(jsonResult):
         'Total Lines': 'lines-valid'
     }
     
-    table = """|{:-^20}|{:-^20}|{:-^20}|{:-^20}|""".format('Metrics', 'Baseline', 'PR', 'Delta')
-    for key in tableMapping.keys():
-        attribute = tableMapping.get(key)
-        pr = jsonResult.get('PR').get(attribute)
-        baseline = jsonResult.get('baseline').get(attribute)
-        delta = jsonResult.get('delta').get(attribute)
+    # table = """|{:-^20}|{:-^20}|{:-^20}|{:-^20}|""".format('Metrics', 'Baseline', 'PR', 'Delta')
+    # for key in tableMapping.keys():
+    #     attribute = tableMapping.get(key)
+    #     pr = jsonResult.get('PR').get(attribute)
+    #     baseline = jsonResult.get('baseline').get(attribute)
+    #     delta = jsonResult.get('delta').get(attribute)
 
-        # line coverage to be displayed as percentage
-        if key is 'Line Coverage':
-            pr *= 100
-            baseline *= 100
-            delta *= 100
+    #     # line coverage to be displayed as percentage
+    #     if key is 'Line Coverage':
+    #         pr *= 100
+    #         baseline *= 100
+    #         delta *= 100
         
-        if key is 'Uncovered Lines':
-            pr = jsonResult.get('PR').get('lines-valid') - jsonResult.get('PR').get('lines-covered')
-            baseline = jsonResult.get('baseline').get('lines-valid') - jsonResult.get('baseline').get('lines-covered')
-            delta = jsonResult.get('delta').get('lines-valid') - jsonResult.get('delta').get('lines-covered')
+    #     if key is 'Uncovered Lines':
+    #         pr = jsonResult.get('PR').get('lines-valid') - jsonResult.get('PR').get('lines-covered')
+    #         baseline = jsonResult.get('baseline').get('lines-valid') - jsonResult.get('baseline').get('lines-covered')
+    #         delta = jsonResult.get('delta').get('lines-valid') - jsonResult.get('delta').get('lines-covered')
         
-        # table.add_row([key, baseline, pr, delta])
-        row = """\n|{: <20}|{: <20}|{: <20}|{: <20}|""".format(key,baseline,pr,delta)
-        table = table + row
+    #     # table.add_row([key, baseline, pr, delta])
+    #     row = """\n|{: <20}|{: <20}|{: <20}|{: <20}|""".format(key,baseline,pr,delta)
+    #     table = table + row
     
     # print table.draw()
     # print type(table)
